2437e37f0d3790f32b4c4a9cc9a90dea4b4d0594
BrendonKupsch1 <Brendon.Kupsch1@marist.edu>
2023-09-07 15:36:54 -0400

    Hex iteration second attempt

--- src/hardware/Memory.ts
+++ src/hardware/Memory.ts
@@ -1,14 +1,30 @@
 import Hardware from './Hardware';
 
-class Memory extends Hardware {
-    private _memory: [number, number] = [0x00, 0x00];
-}
+class Memory {
+    debug: boolean = true;
+    name: string;
 
-function iterateThroughArrayHex(array: [number, number]): string {
-    let hexString: string = '';
-    for (let i = 0; i < array.length; i++) {
-        hexString += array[i].toString(16);
+    private memory: string[] = []; // create private array for memory
+  
+    constructor(size: number) {
+      const length = size * 2; // each element stores 2 hex digits
+      const hexValue = "00".repeat(length); // create hex string of all 0x00
+      for (let i = 0; i < length; i+=2) {
+        this.memory.push(hexValue.substring(i, i+2)); // split hex string into 2 digit chunks and push to memory array
+      }
     }
-    return hexString;
-}
-console.log(iterateThroughArrayHex([0x0a, 0x00]));
+  
+    public initializeMemory() {
+      const length = this.memory.length;
+      for (let i = 0; i < length; i++) {
+        this.memory[i] = "00"; // set each element to 0x00
+      }
+    }
+
+    log(message: string): void {
+        if (this.debug) {
+            const currentTime = new Date().getTime();
+            console.log(); // enter output of hex iteration here to test
+        }
+    }
+  }
