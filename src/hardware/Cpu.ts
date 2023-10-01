3abe220a0459f5df560f1d18a6926395b45b74d3
BrendonKupsch1 <98063893+BrendonKupsch1@users.noreply.github.com>
2023-10-01 23:15:58 +0000

    Added ClockListener and pulse(); to CPU and MEM

--- src/hardware/Cpu.ts
+++ src/hardware/Cpu.ts
@@ -1,8 +1,18 @@
 import Hardware from './Hardware';
+import { CloockListener } from './imp/ClockListener';
 
-class Cpu extends Hardware {
+class Cpu extends Hardware implements CloockListener {
+
+    public cpuClockCount: number = 0;
+
     constructor(id: number) {
        super('Cpu', id);
+    }
+
+    pulse() {
+        let time: number = new Date().getTime();
+        this.cpuClockCount++;
+        console.log("HW " + this.name + " id " + this.id + " " + time + " Received clock pulse - CPU CLock Count: " + this.cpuClockCount);
     }
  
     log(message: string): void {
